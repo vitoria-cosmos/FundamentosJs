a712d00d17308e9eb971f964d6c4ae821f27e54e
vitoria_cosmos <maria.v.cosmo@gmail.com>
2024-01-06 12:47:27 -0300

    conversoesExercises

---
+++ aula02/conversoes.js
@@ -0,0 +1,14 @@
+// tipo de dado 
+// booleanos 
+
+// conversão implícita - converte um tipo de dado em outro
+const numero = 456;
+const numeroString = Number("456a");
+
+console.log(numero === numeroString) //false
+console.log(numero == numeroString) //true
+
+console.log(numero + numeroString)
+
+
+// conversão explícita - Função Number() e String()
