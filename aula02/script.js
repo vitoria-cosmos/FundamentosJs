7e082f8b659cccc473130db39b3ea3d0d5e02030
vitoria_cosmos <maria.v.cosmo@gmail.com>
2024-01-06 12:12:01 -0300

    Class02Exercises

---
+++ aula02/script.js
@@ -0,0 +1,37 @@
+// var
+
+// var altura = 5;
+// var comprimento = 7;
+
+// area = altura * comprimento;
+
+// var area = 5;
+
+// console.log(area)
+
+// let forma = 'retângulo'
+// let altura = 5;
+// let comprimento = 7;
+// let area;
+// if (forma === 'retângulo') {
+//     area = altura * comprimento;
+// } else {
+//     area = (altura * comprimento) / 2;
+// }
+
+// console.log(area)
+
+const forma = 'quadrado';
+const altura = 5;
+const comprimento = 7;
+// const area;
+let area;
+
+if (forma === 'triângulo') {
+    area = altura * comprimento;
+} else {
+    area = (altura * comprimento) / 2;
+}
+
+console.log(area)
+
