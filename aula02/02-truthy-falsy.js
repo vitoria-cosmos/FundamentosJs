605cf50dfb572fbd8e90aa42dc9ee0afd43725be
vitoria_cosmos <maria.v.cosmo@gmail.com>
2024-02-05 16:03:52 -0300

    aula sobre valores verdadeiros e falsos

---
+++ aula02/02-truthy-falsy.js
@@ -0,0 +1,31 @@
+// boolean 
+const usuarioLogado = true;
+const contapaga = false;
+
+// truthy ou falsy
+
+// 0 => false
+// 1 => true 
+
+// console.log(0 == false) - true
+// console.log("" == false) - true 
+// console.log(1 == true) - true
+
+
+// null 
+// undefined 
+
+let minhaVar; // undefined
+let varNull = null; // null 
+
+// console.log(minhaVar)
+// console.log(varNull)
+
+let numero = 3;
+let texto = "Alura";
+
+// console.log(typeof numero) - number
+// console.log(typeof texto) - string
+
+console.log(typeof minhaVar) // undefined
+console.log(typeof varNull) // object
