231b815a32f12b2f4dc7ade1c1e1f263815fff48
vitoria_cosmos <maria.v.cosmo@gmail.com>
2024-02-10 09:11:46 -0300

    Alterações da aula 04.

---
+++ aula04/01-paracoes.js
@@ -0,0 +1,23 @@
+// == (comparação implícita)
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero == texto);
+
+// === (comparação explícita)
+
+console.log(numero === texto);
+
+//typeof
+console.log(typeof numero)
+console.log(typeof texto)
+
+
+// == só compara o valor
+// === compara o valor e o tipo de dado
+
+// conversão explícita = boa prática
+
+Number()
+String()
---
+++ aula04/02-ternario.js
@@ -0,0 +1,16 @@
+const idadeMinima = 18;
+const idadeCliente = 19;
+
+// Com if e else
+
+// if (idadeCliente >= 18) {
+//     console.log("cerveja")
+// } else {
+//     console.log("suco")
+// }
+
+// Operador Ternário
+
+console.log(idadeCliente >= idadeMinima? "cerveja" : "suco")
+
+
