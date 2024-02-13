64e6e67849606ae460d20d9a52b90ab645c81ca5
vitoria_cosmos <maria.v.cosmo@gmail.com>
2024-02-13 15:52:36 -0300

    Aula sobre funções.

---
+++ aula05/01-funcoes.js
@@ -0,0 +1,46 @@
+// let x = "";
+// console.log(x)
+
+// x = "oi";
+
+
+// DECLARAÇÃO DE FUNÇÃO:
+
+// 1) declara a função
+                     //string
+function imprimeTexto(texto) {
+    console.log(texto)
+}
+
+                    // 2 -> valor de soma()
+function imprimeTexto(texto) {
+    console.log(texto) 
+}
+
+
+
+
+
+// 2) executa a função (1 ou + vezes)
+
+imprimeTexto(soma());
+// imprimeTexto("outro texto");
+
+
+// três formas de escrever funções
+
+
+// function soma(){
+//     const resultado = 2 + 2;
+// }
+
+
+function soma(){
+    //outros códigos
+    //vários console.log()
+    return 2 + 2;
+}
+
+// console.log(soma())
+
+
